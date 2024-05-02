3001ca463e2e86bd4be62b66b63fe206c6c8b300
pablo27exe <163430211+pablo27exe@users.noreply.github.com>
2024-05-02 13:30:11 -0600

    Update Program.cs

--- Herencia/Program.cs
+++ Herencia/Program.cs
@@ -27,7 +27,7 @@ namespace Herencia
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            //Application.Run(new Form1());
+            Application.Run(new Form1());
         }
     }
 }
