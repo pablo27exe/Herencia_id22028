b8ed7ab19192dec3f5ec3e73650804fa86ed4e17
pablo27exe <163430211+pablo27exe@users.noreply.github.com>
2024-05-07 20:38:23 -0600

    valores flotantes

--- Herencia/Form1.cs
+++ Herencia/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+using HERENCIA1;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace Herencia
 {
@@ -19,27 +21,61 @@ namespace Herencia
 
         private void button1_Click(object sender, EventArgs e)
         {
-
+            //voy a darle comportamiento a mi boton
+            //voy a utilizar mi clase hija sumar
+            Sumar suma = new Sumar(); //instancie, o
+                                      //hice un nuevo objeto suma
+                                      //llamar al metodo de clase suma operar
+            resultado.Text = suma.operar
+            (float.Parse(valor1.Text), float.Parse(valor2.Text)).ToString();
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-
+            //voy a darle comportamiento a mi boton
+            //voy a utilizar mi clase hija dividir
+            try
+            {
+                Dividir division = new Dividir(); //instancie, o
+                                                  //hice un nuevo objeto resta
+                                                  //llamar al metodo de clase restar operar
+                resultado.Text = division.operar
+                (float.Parse(valor1.Text), float.Parse(valor2.Text)).ToString();
+            }
+            catch(ArgumentException ex) { 
+                MessageBox.Show(ex.Message, "Error",MessageBoxButtons.OK, MessageBoxIcon.Error);
+                valor1.Clear();
+                valor2.Clear();
+            }
         }
 
         private void multiplicar_Click(object sender, EventArgs e)
         {
-
+            //voy a darle comportamiento a mi boton
+            //voy a utilizar mi clase hija multiplicar
+            Multiplicar multiplicar = new Multiplicar(); //instancie, o
+                                                         //hice un nuevo objeto multiplicacion
+                                                         //llamar al metodo de clase multiplicar operar
+            resultado.Text = multiplicar.operar
+            (float.Parse(valor1.Text), float.Parse(valor2.Text)).ToString();
         }
 
         private void restar_Click(object sender, EventArgs e)
         {
-
+            //voy a darle comportamiento a mi boton
+            //voy a utilizar mi clase hija restar
+            Restar resta = new Restar(); //instancie, o
+                                         //hice un nuevo objeto resta
+                                         //llamar al metodo de clase restar operar
+            resultado.Text = resta.operar
+            (float.Parse(valor1.Text), float.Parse(valor2.Text)).ToString();
         }
 
         private void borrar_Click(object sender, EventArgs e)
         {
-
+            valor1.Clear();
+            valor2.Clear();
+            resultado.Clear();
         }
 
         private void label1_Click(object sender, EventArgs e)
