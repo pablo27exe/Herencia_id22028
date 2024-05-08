1f1dc2ed0177c24b97ee006e5eac31e80da67a62
pablo27exe <163430211+pablo27exe@users.noreply.github.com>
2024-05-07 20:40:04 -0600

    valores flotantes y division entre cero

--- Herencia/Program.cs
+++ Herencia/Program.cs
@@ -1,24 +1,115 @@
-﻿using System;
+using Herencia;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Herencia
+namespace HERENCIA1
 {
-    internal static class Program
+    /*clase principal o clase papá que guardará los metodos de lectura de valores y almacenado de valores en variables*/
+    //nombre de clase
+    class operacion
     {
-        //clase principal o clase papá que guardará los metodos de lectura de valores y almacendao de valores en variables
-        //NOMBRE DE CLASE
-        class operación
+        //declaracion de variables que usa la clase
+        //tipo de acceso    tipo de dato    nombre de dato
+        protected float valor1;
+        protected float valor2;
+        protected float resultado;
+
+        //atributos de la clase operación
+        //metodos o fuciones de operacion
+        /*los metodos que se ponen acontinuacón
+         * son las funciones de obtener el valor
+         * de la caja de texto y guardarlo en la 
+         * memoria dentro de la variable indicada*/
+        //constructor de dato valor 1
+        public float Valor1
         {
-            //declaración de variables que usa la clase
-            //tipo de acceso    tipo de dato    nombre de dato
-            protected           int             valor1;
-            protected           int             valor2;
-            protected           int             resultado;
-           
+            get { return valor1; } //vamos por el valor de la caja de texto
+            set { valor1 = value; }//lo guardamos en memoria
         }
+        public float Valor2
+        {
+            get { return valor2; } //vamos por el valor de la caja de texto
+            set { valor2 = value; }//lo guardamos en memoria
+        }
+
+        public float Resultado
+        {
+            get { return resultado; } //vamos por el valor de la caja de texto
+            set { resultado = value; }//lo guardamos en memoria
+        }
+    }
+    //creamos nuestra clase hijo que usara los
+    //metodos de crear dato
+    class Sumar : operacion //los : nos dan permiso de usar lo que tiene operacion
+    {
+        //las clases hijas tambien pueden tener sus propios atributos y métodos
+        //pueden tener variables de uso solamente para la clase hijo
+        //declarar variables de esta clase
+        //atributos de esta clase
+        //metodos de clase
+        public float operar(float v1, float v2)
+        {
+            valor1 = v1; //accemos al constructor de operación
+            valor2 = v2;
+            return resultado = valor1 + valor2;
+        }
+    }
+    class Restar : operacion //los : nos dan permiso de usar lo que tiene operacion
+    {
+        //las clases hijas tambien pueden tener sus propios atributos y métodos
+        //pueden tener variables de uso solamente para la clase hijo
+        //declarar variables de esta clase
+        //atributos de esta clase
+        //metodos de clase
+        public float operar(float v1, float v2)
+        {
+            valor1 = v1; //accemos al constructor de operación
+            valor2 = v2;
+            return resultado = valor1 - valor2;
+        }
+    }
+    class Multiplicar : operacion //los : nos dan permiso de usar lo que tiene operacion
+    {
+        //las clases hijas tambien pueden tener sus propios atributos y métodos
+        //pueden tener variables de uso solamente para la clase hijo
+        //declarar variables de esta clase
+        //atributos de esta clase
+        //metodos de clase
+        public float operar(float v1, float v2)
+        {
+            valor1 = v1; //accemos al constructor de operación
+            valor2 = v2;
+            return resultado = valor1 * valor2;
+        }
+    }
+
+    class Dividir : operacion //los : nos dan permiso de usar lo que tiene operacion
+    {
+        //las clases hijas tambien pueden tener sus propios atributos y métodos
+        //pueden tener variables de uso solamente para la clase hijo
+        //declarar variables de esta clase
+        //atributos de esta clase
+        //metodos de clase
+        public float operar(float v1, float v2)
+        {
+            valor1 = v1; //accemos al constructor de operación
+            valor2 = v2;
+            if (valor1 == 0)
+            {
+                throw new ArgumentException("No se puede dividir por cero");//lanza una advertencia de un error que ocurre cuando se está corriendo el programa.
+            }
+            else if (valor2 == 0)
+            {
+                throw new ArgumentException("No se puede dividir por cero");//lanza una advertencia de un error que ocurre cuando se está corriendo el programa.
+            }
+            return resultado = valor1 / valor2;
+        }
+    }
+    static class Program
+    {
         /// <summary>
         /// Punto de entrada principal para la aplicación.
         /// </summary>
